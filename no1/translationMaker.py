351797c80c1ef346fce29bb5471f7f327d1c3b6d
sorayams <somasa.laquais@gmail.com>
2022-06-16 19:37:16 +0700

    Def Translasi
    update method translasi

--- no1/translationMaker.py
+++ no1/translationMaker.py
@@ -4,22 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 
-# Translasi
-def translasi(object,u,v,th):
-    row,col,depth=np.shape(object)
-    print("Row, Col, Depth = ",row,col,depth)
-    print("u, v, th=",u,v,th)
-
-    obj_out=np.zeros(shape=(row,col,3),dtype=np.uint8)#template for output
-    for i in range(0,row-u-1):
-        for j in range(0,col-v-1):
-            value=object[i,j,0]+object[i,j,1]+object[i,j,2]
-            if value>th:
-                obj_out[i+u,j+v,0]=object[i,j,0]
-                obj_out[i+u,j+v,1]=object[i,j,1]
-                obj_out[i+u,j+v,2]=object[i,j,2]
-    return obj_out
-
+# MODEL
 def createCircleModel(col,row):
     a, b = col/2, row/2
     r = 2
@@ -67,51 +52,142 @@ def createSquareModel(col,row):
     plt.imshow(dummy)
     plt.show()
 
+# TRANSLASI
+def translation(object,u,v,th):
+    row,col,depth=np.shape(object)
+    print("Row, Col, Depth = ",row,col,depth)
+    print("u, v, th=",u,v,th)
+
+    obj_out=np.zeros(shape=(row,col,3),dtype=np.uint8)#template for output
+    for i in range(0,row-u-1):
+        for j in range(0,col-v-1):
+            value=object[i,j,0]+object[i,j,1]+object[i,j,2]
+            if value>th:
+                obj_out[i+u,j+v,0]=object[i,j,0]
+                obj_out[i+u,j+v,1]=object[i,j,1]
+                obj_out[i+u,j+v,2]=object[i,j,2]
+    return obj_out
+
+def bounce(model):
+    u = 1
+    v = 0
+    th = 15
+
+    for i in range(1, 25):
+        model_translated = translation(model, u, v, th)
+        plt.imshow(model_translated)
+        plt.pause(0.01)
+        model = model_translated
+
+    u = -1;
+    v = 0
+    for i in range(1, 51):
+        model_translated = translation(model, u, v, th)
+        plt.imshow(model_translated)
+        plt.pause(0.01)
+        model = model_translated
+
+    plt.show()  # syarat menampilkan
+
+def around(model):
+    u = 1
+    v = 0
+    th = 15
+
+    for i in range(1, 25):
+        model_translated = translation(model, u, v, th)
+        plt.imshow(model_translated)
+        plt.pause(0.01)
+        model = model_translated
+
+    u = -1;
+    v = 0
+    for i in range(1, 51):
+        model_translated = translation(model, u, v, th)
+        plt.imshow(model_translated)
+        plt.pause(0.01)
+        model = model_translated
+
+    plt.show()  # syarat menampilkan
+
+def zigzag(model):
+    u = 1
+    v = 0
+    th = 15
+
+    for i in range(1, 25):
+        model_translated = translation(model, u, v, th)
+        plt.imshow(model_translated)
+        plt.pause(0.01)
+        model = model_translated
+
+    u = -1;
+    v = 0
+    for i in range(1, 51):
+        model_translated = translation(model, u, v, th)
+        plt.imshow(model_translated)
+        plt.pause(0.01)
+        model = model_translated
+
+    plt.show()  # syarat menampilkan
+
+def diagonal(model):
+    u = 1
+    v = 0
+    th = 15
+
+    for i in range(1, 25):
+        model_translated = translation(model, u, v, th)
+        plt.imshow(model_translated)
+        plt.pause(0.01)
+        model = model_translated
+
+    u = -1;
+    v = 0
+    for i in range(1, 51):
+        model_translated = translation(model, u, v, th)
+        plt.imshow(model_translated)
+        plt.pause(0.01)
+        model = model_translated
+
+    plt.show()  # syarat menampilkan
+
+## MAIN PROGRAM ##
 print("Pilih Resolusi")
 print("A. 60 x 60")
 print("B. 80 x 80")
 print("C. 90 x 90")
+
 inputResolusi = input("Pilih Resolusi  : ")
 col=0;row=0
 if inputResolusi.lower() == 'a':
     col=60;row=60
-    print( 'A')
+    print('A')
 elif inputResolusi.lower() == 'b':
     col=80;row=80
-    print( 'B')
+    print('B')
 elif inputResolusi.lower() == 'c':
     col=90;row=90
-    print( 'C')
+    print('C')
 
 print("Pilih Model")
 print("A. Persegi")
 print("B. Lingkar")
+
 inputModel = input("Pilih Model : ")
 if inputModel.lower() == 'a':
     createSquareModel(col,row)
     model = np.load("Square.npy")
+    print('A')
 elif inputModel.lower() == 'b':
     createCircleModel(col,row)
     model = np.load("Circle.npy")
-u=1;v=0;th=15
-row,col,depth=np.shape(model)
-model_translated=translasi(model,u,v,th)
-plt.figure(figsize=(3,3),dpi=200)
-plt.imshow(model)
+    print('B')
 
-
-u = 1; v = 0
-for i in range(1,25):
-    model_translated = translasi(model, u, v, th)
-    plt.imshow(model_translated)
-    plt.pause(0.01)
-    model = model_translated
-
-u = -1; v = 0
-for i in range(1,51):
-    model_translated = translasi(model, u, v, th)
-    plt.imshow(model_translated)
-    plt.pause(0.01)
-    model = model_translated
-
-plt.show() #syarat menampilkan
+inputTranslasi = input("Pilih Translasi : ")
+if inputTranslasi.lower() == 'a':
+    bounce(model)
+elif inputTranslasi.lower() == 'b':
+    around(model)
+elif inputTranslasi.lower() == 'c':
+    zigzag(model)
