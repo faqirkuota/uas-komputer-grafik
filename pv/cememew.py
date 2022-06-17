ca5197eae8725d054c1e69de4024c29969c6c3f2
Isal <isalisal633@gmail.com>
2022-06-17 21:40:54 +0700

    Commit

--- pv/cememew.py
+++ pv/cememew.py
@@ -16,13 +16,18 @@ ypos = [1, 3, 1, 3]
 zpos = [0, 0, 0, 0]
 dx = [1, 1, 1, 1]
 dy = [1, 1, 1, 1]
-dz = [3, 2, 6, 5]
+dz = []
 
 # add bars
 bars = []
 for i in range(1):
-    bars.append(ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color=random.choice(['r', 'g', 'b'])))
-ax.set_title('3D bars')
+    if(dz[i]>=0 and dz[i]<=5):
+        bars.append(ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color='r'))
+    elif(dz[i]>=5 and dz[i]<=7):
+        bars.append(ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color='b'))
+    elif (dz[i] >= 7 and dz[i] <= 10):
+        bars.append(ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color='g'))
+ax.set_title('Matrix Penilaian Siswa')
 
 line_ani = animation.FuncAnimation(fig, update_bars, 20, fargs=[bars], interval=100, blit=False)
 plt.show()
